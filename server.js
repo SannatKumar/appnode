fe43a701ab1e890d8d6371fec52f6060ddeb3616
Raj Kumar <interplaytheheads@gmail.com>
2020-09-18 23:37:00 +0200

    get method added and hello wrold as a response displayed

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app     = express();
 app.listen(3000, function() {
     console.log('Listening on 3000');
 })
+
+//get method is handled with app.get(endpoint, callback)
+//endpoint is the path and callback is a function that takes two object request(req) and response(res) as an arguments.
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
